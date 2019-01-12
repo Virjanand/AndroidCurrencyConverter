5ac586cab45c396a9bf3076b1ff16b55e85f1fce
Virjanand <virjanand@virjanand.com>
2019-01-12 06:57:22 +0100

    Added image, text, decimal number input and button. When clicked converts amount from pounds to dollars

--- app/src/main/java/com/virjanand/currencyconverter/MainActivity.java
+++ app/src/main/java/com/virjanand/currencyconverter/MainActivity.java
@@ -2,8 +2,25 @@ package com.virjanand.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertAmountToDollars(View view) {
+        double exchangeRate = 1.29;
+
+        EditText amountText = (EditText) findViewById(R.id.amountEditNumberDecimal);
+        double amountInPounds = Double.parseDouble(amountText.getText().toString());
+
+        double amountInDollars = exchangeRate * amountInPounds;
+
+        Toast.makeText(getApplicationContext(),
+                "$ " + String.format("%.2f",amountInDollars),
+                Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
